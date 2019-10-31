e6af9fab2126bbdeadf27f52af51eaa1396bed00
David Naim <DNaim@spartaglobal.com>
2019-10-31 11:15:24 +0000

    added update to recipes

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,9 +4,15 @@
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
+apt_update 'update_sources' do
+  action :update
+end
+
 package 'nginx'
+
 service "nginx" do
   action [:enable, :start]
 end
+
 package 'nodejs'
 package 'npm'
